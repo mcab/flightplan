8af440f9dded2bd0ed46b6f9bee07842b1551cd4
rc-li <husky.li.ruicheng@gmail.com>
2019-04-20 23:41:58 -0400

    just attempts to make the new tab work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>test</h2>
+    <h1>not sure why Forms is not loaded, guessing that the route is not fully configured</h1>
+    <h2>so the webpage does refresh itself when the autocompilation is triggered</h2>
+    <h1>just got an warning</h1>
   </div>
 </template>
--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="forms">
     <h1>This is an about page</h1>
+    <h2>show something!</h2>
+    <h1>something</h1>>
   </div>
 </template>
 
