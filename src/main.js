03cfe00e501095ae98b10441cab559d83d72ea42
Mark Cabanero <me@markcabanero.com>
2019-04-30 08:47:11 -0400

    Add default styles to Ionic components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
 import Ionic from "@ionic/vue";
+import "@ionic/core/css/core.css";
+import "@ionic/core/css/ionic.bundle.css";
+import "./registerServiceWorker";
 
 Vue.use(Ionic);
 Vue.config.productionTip = false;
