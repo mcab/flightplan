565c2e17bd0b63b5dbb51ac8ee4fdf5e33aa3aa6
Mark Cabanero <me@markcabanero.com>
2019-05-03 00:18:22 -0400

    Linting issues

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,8 +14,12 @@
           <ion-list>
             <ion-item>
               <ion-grid>
-                <ion-row><h2>{{ slide.title }}</h2></ion-row>
-                <ion-row v-if="slide.subtitle"><h6>{{ slide.subtitle }}</h6></ion-row>
+                <ion-row
+                  ><h2>{{ slide.title }}</h2></ion-row
+                >
+                <ion-row v-if="slide.subtitle"
+                  ><h6>{{ slide.subtitle }}</h6></ion-row
+                >
               </ion-grid>
             </ion-item>
             <ion-item v-for="(item, index) in slide.items" :key="index">
@@ -51,7 +55,7 @@ export default {
             "Bats make up 25% of the more than 5,400 mammal species worldwide; however, only 8 bat species are native to Connecticut.",
             "Bats worldwide have varied diets, from flower nectar to frogs and fish. All CT bats eat insects.",
             "A single little brown bat can eat up to 1000 mosquitoes per hour.",
-            "Only 3 species of bats actually drink blood, and they prefer other animal blood over human blood. None of them are present in the United States.",
+            "Only 3 species of bats actually drink blood, and they prefer other animal blood over human blood. None of them are present in the United States."
           ]
         },
         slide3: {
@@ -61,7 +65,7 @@ export default {
             "Bats save the corn-growing industry alone more than 1 billion dollars in pest control annually through the number of insects they eat.",
             "Less than 1% of all bats carry rabies, infecting 1 or 2 people in the United States annually. You cannot get rabies from just being near a bat, and there are few cases of bats infecting pets or livestock.",
             "Bats are actually very clean animals; when they are not eating or sleeping, they are usually grooming themselves.",
-            "Bats do not get caught in people’s hair; they are skilled at dodging obstacles (including people) with a technique known as echolocation, and will only come near a person to catch nearby insects.",
+            "Bats do not get caught in people’s hair; they are skilled at dodging obstacles (including people) with a technique known as echolocation, and will only come near a person to catch nearby insects."
           ]
         },
         slide4: {
@@ -69,7 +73,7 @@ export default {
           title: "Threats to Bats",
           subtitle: "Climate Change",
           items: [
-            "Climate change has affected everything from hibernation periods to the abundance of prey for bats. Many bats have also not adapted to handle the rising worldwide temperatures, which can be deadly in the heat of summer.",
+            "Climate change has affected everything from hibernation periods to the abundance of prey for bats. Many bats have also not adapted to handle the rising worldwide temperatures, which can be deadly in the heat of summer."
           ]
         },
         slide5: {
@@ -77,7 +81,7 @@ export default {
           title: "Threats to Bats",
           subtitle: "Wind Turbines",
           items: [
-            "Wind turbine can have devastating effects on bats, to the extent that between 600,000 and 900,000 bats are killed each year from either crashing into the turbines, or even flying too close (the sudden change in air pressure can cause fatal lung injuries).",
+            "Wind turbine can have devastating effects on bats, to the extent that between 600,000 and 900,000 bats are killed each year from either crashing into the turbines, or even flying too close (the sudden change in air pressure can cause fatal lung injuries)."
           ]
         },
         slide6: {
@@ -85,7 +89,7 @@ export default {
           title: "Threats to Bats",
           subtitle: "Habitat Destruction",
           items: [
-            "The clearing of natural habitats has caused a decline in bat populations worldwide. Certain caves have also had fires set in them, killing thousands of roosting bats.",
+            "The clearing of natural habitats has caused a decline in bat populations worldwide. Certain caves have also had fires set in them, killing thousands of roosting bats."
           ]
         },
         slide7: {
@@ -94,7 +98,7 @@ export default {
           subtitle: "White Nose Syndrome",
           items: [
             "However, one of the most deadly threats to bats today is a fungal disease known as White Nose Syndrome (WNS). It is believed to have been inadvertently introduced to North America from Europe, where the native bats are immune.",
-            "Many species have been severely affected by the disease since its introduction in early 2006. It is most common in eastern states, but cases have been reported in the Midwest and West, as well as several Canadian provinces.",
+            "Many species have been severely affected by the disease since its introduction in early 2006. It is most common in eastern states, but cases have been reported in the Midwest and West, as well as several Canadian provinces."
           ]
         },
         slide8: {
@@ -103,7 +107,7 @@ export default {
           subtitle: "White Nose Syndrome",
           items: [
             "WNS gets its name from the white fungus that grows on the muzzles and wings of infected bats. The disease causes bats to awaken and leave their hibernation sites early, which causes a loss of vital fat reserves, leading them to starve or freeze to death.",
-            "WNS primarily affects hibernating bat species, which includes five out of the eight CT species – the big brown bat, little brown bat, long-eared bat, Indiana bat, and tri-colored bat.",
+            "WNS primarily affects hibernating bat species, which includes five out of the eight CT species – the big brown bat, little brown bat, long-eared bat, Indiana bat, and tri-colored bat."
           ]
         },
         slide9: {
@@ -112,7 +116,7 @@ export default {
           subtitle: "White Nose Syndrome",
           items: [
             "Many species have seen between 90 and 100 percent mortality rates in certain areas, with some being reduced to less than 1 percent of their pre-WNS population sizes.",
-            "While WNS has had no recorded effects on humans, people are advised to stay away from caves closed for WNS to prevent the fungus from getting on clothes or caving gear, which could spread the disease to a healthy population of bats. The public should avoid caves with non-infected hibernating bats as well, in order to avoid disturbing them.",
+            "While WNS has had no recorded effects on humans, people are advised to stay away from caves closed for WNS to prevent the fungus from getting on clothes or caving gear, which could spread the disease to a healthy population of bats. The public should avoid caves with non-infected hibernating bats as well, in order to avoid disturbing them."
           ]
         },
         slide10: {
@@ -133,9 +137,9 @@ export default {
             "Bat houses can vary greatly in size, from less than a foot tall to several feet. The number of 'rooms' varies as well, with several usually being preferable over one.",
             "Houses with multiple chambers are better suited to maternity colonies (mothers with their young), while single-chamber houses are more like ‘bachelor pads’ for bats."
           ]
-        },
+        }
       }
-    }
+    };
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,9 +12,21 @@
       <h2>Home Page</h2>
       <p>This is Team's 36 Citizen Science Bat House Monitoring Application!</p>
       <p>Many thanks for taking the time to look and view this project.</p>
-      <p>The main idea of this project is data aggregation: if we can encourage users to contribute data, this helps immensely for endangered bat species across the state.</p>
-      <p>With all the data being stored in a PostgreSQL database, it's a matter of extracting that data, and correlating it with other factors. That is left as an (extensive) future project.</p>
-      <p>We know there's plenty to think about in this world, but not having to deal with insects and pests outside of your home is always an appreciated thought.</p>
+      <p>
+        The main idea of this project is data aggregation: if we can encourage
+        users to contribute data, this helps immensely for endangered bat
+        species across the state.
+      </p>
+      <p>
+        With all the data being stored in a PostgreSQL database, it's a matter
+        of extracting that data, and correlating it with other factors. That is
+        left as an (extensive) future project.
+      </p>
+      <p>
+        We know there's plenty to think about in this world, but not having to
+        deal with insects and pests outside of your home is always an
+        appreciated thought.
+      </p>
     </ion-content>
   </div>
 </template>
